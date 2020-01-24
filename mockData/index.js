b279e91f941e5531daf987f3ffac3619c5f9bb3f
Takanori Sono <takanori.sono@gmail.com>
2020-01-23 21:55:50 -0800

    Update get restaurant function to search by name or uid

--- mockData/index.js
+++ mockData/index.js
@@ -2,32 +2,30 @@ var mongoose = require('mongoose');
 const db = mongoose.connect('mongodb://localhost/mockRestaurants', { useNewUrlParser: true, useUnifiedTopology: true });
 const Restaurant = require('./data.js');
 
-var getRestaurantByID = function(uid, callback) {
-  Restaurant.findOne({ uid: uid})
+var getRestaurantByID = function(search, callback) {
+  var query;
+  if (typeof(search) === 'string') {
+    //if type is string, query with name
+    query = {name: search}
+  } else {
+    //should query search with uid
+    query = {uid: search}
+  }
+  
+  //search with query object
+  Restaurant.findOne(query)
     .then((data) => {
       callback(data);
     })
     .catch(err => console.log(err))
     .then(() => {
       mongoose.connection.close();
-    })
-  
-  // Restaurant.find({ uid: uid}, (err, data) => {
-  //   if (err) {return err}
-  //   mongoose.connection.close();
-  //   return data;
-    
-  // });
-}
-
-var test = getRestaurantByID(1, (data) => console.log(data.name));
-var getRestaurantName = function(uid) {
-
+    }) 
 }
 
 
-
-
+// var test = getRestaurantByID('Mendocino Farms', (data) => console.log(data));
+// var test = getRestaurantByID(1, (data) => console.log(data));
 
 
 module.exports = db;
